fd581ec53b0c1b636c80942e6085a528ea32c05d
telows <lolryichylol@hotmail.com>
2019-08-22 07:52:32 -0700

    added minor windows tests

--- mass_file_downloader/test.py
+++ mass_file_downloader/test.py
@@ -1,6 +1,7 @@
 
 from mass_file_downloader import create_folder
 import os
+from tkinter import *
 
 
 #teststuff
@@ -8,7 +9,10 @@ pattern = "(?<!\"fileThumb\" )(href=\"//is2.4chan.org/.{1,22}\")"
 url = "http://boards.4channel.org/c/thread/3510179"
 path = "/home/ryan/Pictures/test/"
 
+wpath ="C:\\Users\\XPS\\Documents/test/"
 
-if(os.path.exists(path) == False):
-    create_folder(path)
+if(os.path.exists(wpath) == False):
+    create_folder(wpath)
     print("1")
+
+
